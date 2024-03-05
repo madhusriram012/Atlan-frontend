f8fd4c159439247fed4e683141958bcc28d6bc5c
madhusriram012 <srirammadhu100@gmail.com>
2024-03-05 12:54:59 +0530

    Added BrowserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,44 @@
-import React,{useContext} from 'react';
-import './App.css';
-import NavBar from './components/Navbar/Navbar';
-import Home from './components/Home/Home';
-import {Route, Routes} from "react-router-dom";
-import Explore from './components/Explore/Explore';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ModelDetail from './components/ModelDetails/ModelDetail';
-import RegisterPage from './components/Register/Register';
-import SignPage from './components/SignIn/SignIn';
-import CreateModelCard from './components/CreateModel/createModel';
+import React, { useContext } from "react";
+import "./App.css";
+import NavBar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Explore from "./components/Explore/Explore";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ModelDetail from "./components/ModelDetails/ModelDetail";
+import RegisterPage from "./components/Register/Register";
+import SignPage from "./components/SignIn/SignIn";
+import CreateModelCard from "./components/CreateModel/createModel";
 
-import NotFoundPage from './components/NotFound/NotFoundPage';
-import { NetworkContext } from './components/context/NetworkContext';
-import Internetconnection from './components/InternetConnection/Internetconnection';
-
+import NotFoundPage from "./components/NotFound/NotFoundPage";
+import { NetworkContext } from "./components/context/NetworkContext";
+import Internetconnection from "./components/InternetConnection/Internetconnection";
 
 const App = () => {
   const isOnline = useContext(NetworkContext);
   if (!isOnline) {
-    return <Internetconnection/>
+    return <Internetconnection />;
   }
   return (
     <div className="root">
-    <div className="page-container">
-        <NavBar/>
-      <div className="content-wrap">
-        <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route path='/explore' element={<Explore />} />
-            <Route path='/details/:title' element={<ModelDetail/>} />
-            <Route path='/signin' element={<SignPage/>} />
-            <Route path='/register' element={<RegisterPage/>} />
-            <Route path='/create-model' element={<CreateModelCard/>} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
+      <div className="page-container">
+        <NavBar />
+        <div className="content-wrap">
+          <BrowserRouter>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/explore" element={<Explore />} />
+              <Route path="/details/:title" element={<ModelDetail />} />
+              <Route path="/signin" element={<SignPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/create-model" element={<CreateModelCard />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
       </div>
-   </div>
-   </div>
+    </div>
   );
-}
+};
 
 export default App;
